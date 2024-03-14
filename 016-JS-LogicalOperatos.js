b480defc5b1f71b2eaf951d8fe567eb8a72020b9
chanchalpatel51 <124054642+chanchalpatel51@users.noreply.github.com>
2024-03-14 15:59:58 +0530

    Logical operators

---
+++ 016-JS-LogicalOperatos.js
@@ -0,0 +1,6 @@
+{
+    let x = 6, y = 3;
+    document.getElementById("demo1").innerHTML = (x<10)&&(y>1);
+    document.getElementById("demo2").innerHTML = (x==5)||(y==5);
+    document.getElementById("demo3").innerHTML = !(x==y);
+}
