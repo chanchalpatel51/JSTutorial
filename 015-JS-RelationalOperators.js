1b928121ec15a68219d5bdc89101c099623d86fc
chanchalpatel51 <124054642+chanchalpatel51@users.noreply.github.com>
2024-03-14 13:48:00 +0530

    Relational operators

---
+++ 015-JS-RelationalOperators.js
@@ -0,0 +1,16 @@
+{
+    let x = 5;
+    document.getElementById("demo1").innerHTML = (x==8);
+    document.getElementById("demo2").innerHTML = (x==5);
+    document.getElementById("demo3").innerHTML = (x=="5");
+    document.getElementById("demo4").innerHTML = (x===5);
+    document.getElementById("demo5").innerHTML = (x==="5");
+    document.getElementById("demo6").innerHTML = (x!=8);
+    document.getElementById("demo7").innerHTML = (x!==5);
+    document.getElementById("demo8").innerHTML = (x!=="5");
+    document.getElementById("demo9").innerHTML = (x!==8);
+    document.getElementById("demo10").innerHTML = (x>8);
+    document.getElementById("demo11").innerHTML = (x<8);
+    document.getElementById("demo12").innerHTML = (x>=8);
+    document.getElementById("demo13").innerHTML = (x<=8);
+}
