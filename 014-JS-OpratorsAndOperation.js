232b4498e9ffa0d0cb1cd382e54eed9f3c5c844a
chanchalpatel51 <124054642+chanchalpatel51@users.noreply.github.com>
2024-03-14 13:13:57 +0530

    Aritmatic operations

---
+++ 014-JS-OpratorsAndOperation.js
@@ -0,0 +1,42 @@
+//Oprators and operation
+{
+    let a = 5, b = 2;
+    let c = a + b;                  //Addition oprator 
+    document.getElementById("demo1").innerHTML = c;
+}
+
+{
+    let x = 5, y = 2;
+    let z = x - y;                  //Substraction oprator 
+    document.getElementById("demo2").innerHTML = z;
+}
+
+{
+    let x = 5, y = 2;
+    let z = x * y;                  //Multiplication operator
+    document.getElementById("demo3").innerHTML = z;
+}
+
+{
+    let x = 5, y = 2;               
+    let z = x / y;                  //Division operator
+    document.getElementById("demo4").innerHTML = z;
+}
+
+{
+    let x = 5, y = 2;
+    let z = x % y;                      //Modulo operator
+    document.getElementById("demo5").innerHTML = z;
+}
+
+{
+    let x = 5;
+    x++;                            //Increament operator
+    document.getElementById("demo6").innerHTML = x;
+}
+
+{
+    let x = 5;
+    x--;                            //decreament operator
+    document.getElementById("demo7").innerHTML = x;
+}
