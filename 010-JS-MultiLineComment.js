49f4b67f97af3185ad026bd9b228891625463a80
chanchalpatel51 <124054642+chanchalpatel51@users.noreply.github.com>
2024-03-14 08:41:30 +0530

    Add multiple line comment

---
+++ 010-JS-MultiLineComment.js
@@ -0,0 +1,8 @@
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+*/
+document.getElementById("myH").innerHTML = "JavaScript comments";
+
+document.getElementById("myP").innerHTML = "My first paragraph";
